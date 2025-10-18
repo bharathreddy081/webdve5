6e8ae449b92020d889cbef4184ab3edf88ea1bf2
bharathreddy081 <39465709+bharathreddy081@users.noreply.github.com>
2025-10-18 23:17:08 +0530

    Update google.test.js

--- test/specs/google.test.js
+++ test/specs/google.test.js
@@ -16,6 +16,8 @@ aws_access_key_id = AKIAYVP4CIPPCMH4EAHN
 aws_secret_access_key = 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
 output = json
 region = us-east-2
+
+password=hsgsgw
         
     })
 })
