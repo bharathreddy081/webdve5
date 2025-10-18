13ace0d741788ba1ea6fc6390b49c41c10e34cf5
bharathreddy081 <39465709+bharathreddy081@users.noreply.github.com>
2025-10-18 23:11:35 +0530

    Update google.test.js

--- test/specs/google.test.js
+++ test/specs/google.test.js
@@ -10,5 +10,12 @@ describe('DuckDuckGo search', () => {
         const title = browser.getTitle()
         console.log('Title is: ' + title)
         // outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+
+[default]
+aws_access_key_id = AKIAYVP4CIPPCMH4EAHN
+aws_secret_access_key = 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
+output = json
+region = us-east-2
+        
     })
 })
